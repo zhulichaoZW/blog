371d711a52f751f73aefcd6a0ac07020858640c0
zhulichao <zhulichao@zhiwen.me>
2018-07-22 23:14:43 +0800

    修改个人信息

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,32 +1,28 @@
 const colors = require("../../src/styles/colors");
 
 module.exports = {
-  siteTitle: "PersonalBlog - a blog starter for GatsbyJS", // <title>
-  shortSiteTitle: "PersonalBlog GatsbyJS Starter", // <title> ending for posts and pages
-  siteDescription: "PersonalBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
+  siteTitle: "朱立超的个人博客", // <title>
+  shortSiteTitle: "朱立超的个人博客", // <title> ending for posts and pages
+  siteDescription: "个人博客",
+  siteUrl: "https://www.zhulichao.club",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "朱立超",
+  authorTwitterAccount: "朱立超",
   // info
-  infoTitle: "greg lobinski",
-  infoTitleNote: "personal blog",
+  infoTitle: "朱立超",
+  infoTitleNote: "个人博客",
   // manifest.json
-  manifestName: "PersonalBlog - a blog starter for GatsbyJS",
-  manifestShortName: "PersonalBlog", // max 12 characters
+  manifestName: "朱立超的个人博客",
+  manifestShortName: "朱立超的个人博客", // max 12 characters
   manifestStartUrl: "/",
   manifestBackgroundColor: colors.background,
   manifestThemeColor: colors.background,
   manifestDisplay: "standalone",
   // contact
-  contactEmail: "john@doe.com",
+  contactEmail: "zhulichaoZW@zhiwen.me",
   // social
-  authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
-  ]
+  authorSocialLinks: [{ name: "github", url: "https://github.com/zhulichaoZW" }]
 };
